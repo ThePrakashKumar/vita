3e6aba05d23c6545dcaae58c596683fe2c1c16cc
theprakashkumar <prakashcodes@outlook.com>
2024-04-03 09:14:40 +0530

    feat: add server actions files

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,6 +1,9 @@
 "use server";
 
 import * as auth from "@/auth";
+export { createComment } from "./create-comment";
+export { createPost } from "./create-post";
+export { createTopic } from "./create-topic";
 
 export async function signIn() {
   return auth.signIn("github");
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import { Button } from "@nextui-org/react";
-import * as actions from "@/actions";
+import { signIn } from "@/actions/signIn";
 import { auth } from "@/auth";
 import Profile from "@/components/Profile";
 
@@ -7,7 +7,7 @@ export default async function Home() {
   const session = await auth();
   return (
     <div>
-      <form action={actions.signOut}>
+      <form action={signIn}>
         <Button type="submit"> {session?.user ? "Sing Out" : "Sign In"}</Button>
       </form>
       {!session?.user ? (
