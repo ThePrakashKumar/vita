3d8a7005065a1b04ed945846e8b273ebf60ce053
theprakashkumar <prakashcodes@outlook.com>
2024-04-22 19:08:40 +0530

    feat: add error validation in create topic form + feat: add add topicfeatures

--- src/actions/create-topic.ts
+++ src/actions/create-topic.ts
@@ -1,8 +1,89 @@
 "use server";
+import { auth } from "@/auth";
+import { db } from "@/db";
+import type { Topic } from "@prisma/client";
+import { redirect } from "next/navigation";
+import paths from "@/paths";
+import { z } from "zod";
+import { revalidatePath } from "next/cache";
 
-export async function createTopic(formData: FormData) {
+const createTopicSchema = z.object({
+  name: z
+    .string()
+    .min(3)
+    .regex(/[a-z-]/, {
+      message: "Must be lower cases or dashes without spaces!",
+    }),
+  description: z.string().min(10),
+});
+
+interface CreateTopicFormState {
+  errors: {
+    name?: [];
+    description?: [];
+    _form?: string[];
+  };
+}
+
+export async function createTopic(
+  formState: CreateTopicFormState,
+  formData: FormData
+): Promise<CreateTopicFormState> {
   const name = formData.get("name");
   const description = formData.get("description");
 
-  console.log({ name, description });
+  // safeParse will take the data and do the validation check if there is validation error then it will have those error in the returned object
+  const result = createTopicSchema.safeParse({
+    name,
+    description,
+  });
+
+  if (!result.success) {
+    return {
+      errors: result.error.flatten().fieldErrors as {
+        name?: [];
+        description?: [];
+      },
+    };
+  }
+
+  // shoe error when user is not logged in
+  const session = await auth();
+
+  if (!session || !session.user) {
+    return {
+      errors: {
+        _form: ["You must be logged in!"],
+      },
+    };
+  }
+
+  // create topic
+  let topic: Topic;
+  try {
+    topic = await db.topic.create({
+      data: {
+        // when zod validation goes right we would get data
+        slug: result.data.name,
+        description: result.data.description,
+      },
+    });
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      return {
+        errors: {
+          _form: [err.message],
+        },
+      };
+    } else {
+      return {
+        errors: {
+          _form: ["Something went wrong"],
+        },
+      };
+    }
+  }
+
+  revalidatePath("/");
+  redirect(paths.topicShow(topic.slug));
 }
--- src/components/topics/CreateTopic.tsx
+++ src/components/topics/CreateTopic.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { createTopic } from "@/actions";
 import {
   Button,
@@ -7,8 +8,10 @@ import {
   Input,
   Textarea,
 } from "@nextui-org/react";
+import { useFormState } from "react-dom";
 
 const CreateTopic = () => {
+  const [formState, action] = useFormState(createTopic, { errors: {} });
   return (
     <Popover placement="left">
       <PopoverTrigger>
@@ -16,8 +19,8 @@ const CreateTopic = () => {
       </PopoverTrigger>
       <PopoverContent>
         <form
-          className="flex gap-4 py-2 flex-col align-center"
-          action={createTopic}
+          className="flex gap-4 py-2 w-80 flex-col align-center"
+          action={action}
         >
           <h3 className="text-lg ">Crate a Topic</h3>
           <Input
@@ -25,13 +28,22 @@ const CreateTopic = () => {
             label="Topic"
             labelPlacement="outside"
             placeholder="name"
+            isInvalid={!!formState.errors.name}
+            errorMessage={formState.errors.name?.join(", ")}
           />
           <Textarea
             name="description"
             label="Description"
             labelPlacement="outside"
             placeholder="description"
+            isInvalid={!!formState.errors.description}
+            errorMessage={formState.errors.description?.join(", ")}
           />
+          {formState.errors._form && (
+            <div className="p-2 rounded bg-red-100 border border-red-400">
+              {formState.errors._form.join(", ")}
+            </div>
+          )}
           <Button type="submit" color="primary">
             Submit
           </Button>
